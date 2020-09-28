2d1e76e4711967ac88b3f790ca5ebad62958ee24
jmg <ninano1330@gmail.com>
2020-09-29 02:07:45 +0900

    20200929 02

--- src/solving/Test.java
+++ src/solving/Test.java
@@ -1,64 +1,103 @@
 package solving;
 
-public class Test {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Test {
 	public static void main(String[] args) {
-		//TentoThree();
-		solution(5);
+		int[] priorities = {2,1,3,2};
+		int location = 1;
+		
+		
+		List list = new LinkedList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		
+		System.out.println("size : " + list.size());
+		System.out.println(list.get(1));
+		
+		list.remove(2);
+		
+		System.out.println("size : " + list.size());
+		
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+		
+		
+//		solution(priorities, location);
 	}
 	
-	public static String solution(int n) {    
-		String answer = "";
-		
-		
-		while(n>0) {
-			if(n%3==0) {
-				answer = "4" + answer;
-			}
-			answer = n%3 + answer;
-			n = n/3;
-			System.out.println("answer : " + answer);
-			System.out.println("num : " + n);
-		}	
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        List<Integer> prioritiesList = new LinkedList<Integer>();
+        
+        for(int i=0; i<priorities.length; i++) {
+        	prioritiesList.add(priorities[i]);
+        	System.out.println(prioritiesList.get(i));
+        }
+        
+        int max = 0;
+        List<Integer> locationList = new LinkedList<Integer>();
+        
+        while(!prioritiesList.isEmpty()) {
+        	for(int i=0; i<prioritiesList.size(); i++) {
+        		max = prioritiesList.get(i);
+        		for(int j=i+1; j<prioritiesList.size(); j++) {
+        			if(prioritiesList.get(i) < prioritiesList.get(j)) {
+        				max = prioritiesList.get(j);
+        			}
+        		}
+        		if(max == prioritiesList.get(i)) {
+        			//locationList.add(prioritiesList.get(i));
+        			locationList.add(i);
+        			prioritiesList.remove(i);
+        		}
+        	}
+        }
+        
+        for(int i=0; i<locationList.size(); i++) {
+        	System.out.println(locationList.get(i));
+        }
+        
         return answer;
     }
-	
-	public static void TentoThree(){
-		int num = 9;
-		String answer = "";
-		
-		
-		while(num>0) {
-			answer = num%3 + answer;
-			num = num/3;
-			System.out.println("answer : " + answer);
-			System.out.println("num : " + num);
-		}
-
-		
-	}
-	
-
 }
-
 
 
 /*
 
-124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+문제 설명
+일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄합니다. 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있습니다. 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다. 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
 
-124 나라에는 자연수만 존재합니다.
-124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
-예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다.
-10진법	124 나라	10진법	124 나라
-1		1		6		14
-2		2		7		21
-3		4		8		22
-4		11		9		24
-5		12		10		41
-자연수 n이 매개변수로 주어질 때, n을 124 나라에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
+1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+3. 그렇지 않으면 J를 인쇄합니다.
+예를 들어, 4개의 문서(A, B, C, D)가 순서대로 인쇄 대기목록에 있고 중요도가 2 1 3 2 라면 C D A B 순으로 인쇄하게 됩니다.
+
+내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 알고 싶습니다. 위의 예에서 C는 1번째로, A는 3번째로 인쇄됩니다.
+
+현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성해주세요.
 
 제한사항
-n은 500,000,000이하의 자연수 입니다.
+현재 대기목록에는 1개 이상 100개 이하의 문서가 있습니다.
+인쇄 작업의 중요도는 1~9로 표현하며 숫자가 클수록 중요하다는 뜻입니다.
+location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
+입출력 예
+priorities	location	return
+[2, 1, 3, 2]	2	1
+[1, 1, 9, 1, 1, 1]	0	5
+입출력 예 설명
+예제 #1
+
+문제에 나온 예와 같습니다.
+
+예제 #2
+
+6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
 
 */
