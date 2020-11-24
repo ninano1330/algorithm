57ae1b9f5774ba1f76329e0ab16174a05c3f8fb3
jmg <ninano1330@gmail.com>
2020-11-24 21:23:08 +0900

    long time no see

---
+++ src/doit/Test.java
@@ -0,0 +1,31 @@
+package doit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a = 10; int b = 30; int c = 20;
+		System.out.println(min3(a,b,c));
+		System.out.println(max3(a,b,c));
+	}
+
+	public static int min3(int a, int b, int c) {
+		ArrayList<Integer> al = new ArrayList<>();
+		al.add(a);
+		al.add(b);
+		al.add(c);
+		
+		return Collections.min(al);
+	}
+	
+	public static int max3(int a, int b, int c) {
+		ArrayList<Integer> al = new ArrayList<>();
+		al.add(a);
+		al.add(b);
+		al.add(c);
+		
+		return Collections.max(al);
+	}
+}
